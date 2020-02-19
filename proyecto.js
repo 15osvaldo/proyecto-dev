3615e2fe49bc976d90fd8ce0a5badf7f3a086c9f
15osvaldo <valdosalazar@gmail.com>
2020-02-18 21:18:42 -0600

    subiendo copia de proyecto

---
+++ proyecto.js
@@ -0,0 +1,18 @@
+function esconder()
+{
+    document.getElementById("boton").classList.add("ocultar")
+
+}
+
+function mostrar()
+{
+    document.getElementById("micolumna").classList.remove("hidden")
+}
+
+function openNav() {
+    document.getElementById("mifondo").style.width = "50%";
+  }
+
+  function closeNav() {
+    document.getElementById("mifondo").style.width = "0";
+  }
